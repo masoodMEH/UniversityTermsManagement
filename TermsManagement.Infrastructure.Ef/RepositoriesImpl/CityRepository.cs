c1650c40d343ffee83ad49ba7a1fd36bee6c22ba
/\/\@SoOd <42731850+masoodMEH@users.noreply.github.com>
2025-04-19 02:00:00 +0330

    adding dbcontext to repositories

--- TermsManagement.Infrastructure.Ef/RepositoriesImpl/CityRepository.cs
+++ TermsManagement.Infrastructure.Ef/RepositoriesImpl/CityRepository.cs
@@ -5,4 +5,10 @@ namespace TermsManagement.Infrastructure.Ef.RepositoriesImpl;
 
 public class CityRepository : GenericRepository<int, City>, ICityRepository
 {
+    private readonly TermsManagement_Context _context;
+
+    public CityRepository(TermsManagement_Context context) : base(context)
+    {
+        _context = context;
+    }
 }
--- TermsManagement.Infrastructure.Ef/RepositoriesImpl/StateRepository.cs
+++ TermsManagement.Infrastructure.Ef/RepositoriesImpl/StateRepository.cs
@@ -5,4 +5,9 @@ namespace TermsManagement.Infrastructure.Ef.RepositoriesImpl;
 
 public class StateRepository : GenericRepository<int, State>, IStateRepository
 {
+    private readonly TermsManagement_Context _context;
+    public StateRepository(TermsManagement_Context context) : base(context)
+    {
+        _context = context;
+    }
 }
